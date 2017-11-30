3e0f9039a32672d275e041459f34154e463b14f2
Garling <sgarling@au10301.au.deloitte.com>
2017-11-30 11:56:15 +1100

    adding sfmc auth token

--- app_sfmc_functions.py
+++ app_sfmc_functions.py
@@ -16,7 +16,7 @@ def SFMC_authenticate():
 
 	response = requests.post(SFMC_AUTH_URL,headers=headers,data=json.dumps(body))
 	responseStatus = response.status_code
-	if responseStatus === requests.code.ok:
+	if responseStatus == requests.code.ok:
 		responseData = json.load(response)
 		authToken = responseData[accessToken]
 		os.environ.set('SFMC_ACCESS_TOKEN',authToken)
