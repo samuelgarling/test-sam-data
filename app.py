469f20bc856b028106a3e24e4edc1e1b0e6678b6
Garling <sgarling@AU10301.local>
2017-11-30 17:09:04 +1100

    fml

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def testId(basicId):
 def SFMCAuthTest():
 	current_auth = db.session.query(sfmc_access).order_by(sfmc_access.id.desc()).limit(1)
 
-	if len(current_auth) == 1:
+	if current_auth.length == 1:
 		token = current_auth
 	else:
 		token = app_sfmc_functions.SFMC_authenticate()
